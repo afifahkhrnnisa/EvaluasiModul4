8134d595214961585923dbcdc20dc98affa36f61
afifahkhrnnisa <khairunnisaaafifahh@gmail.com>
2024-04-27 15:39:15 +0700

    Finished Project

--- app/src/main/java/com/example/b1_prak4_13020220008/MainActivity.java
+++ app/src/main/java/com/example/b1_prak4_13020220008/MainActivity.java
@@ -62,11 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 dbHelper.close();
             }
         });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 
     private void simpanData() {
